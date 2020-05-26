e05d962d0983be60e907d71c17a3628bc53cde0c
ginaesps <58644384+ginaesps@users.noreply.github.com>
2020-05-25 20:50:16 -0500

    Update ordenBusqueda.js

--- ordenBusqueda.js
+++ ordenBusqueda.js
@@ -1,8 +1,3 @@
-/* DUDAS
-  - cada método de ordenamiento debería tener randomFill al principio considerando que antes de cada uno lo pongo?
-  - comentario en tercera línea de método show()
-  - como puedo comprobar que el vector tenga información en sus posiciones al inicio de cada método de búsqueda/ordenamiento?
-*/
 class Info {
   constructor(size) {
     this.size = size;
@@ -15,66 +10,88 @@ class Info {
   Show() {
     if (this.array === null) return null;
     else {
-      let info = 0; // como puedo inicializar esta variable para evitar que al inicio del vector haya un 0?
+      let info = " ";
       for (let i = 0; i < this.size; i++) info += this.array[i] + "  ";
       return info;
     }
   }
   Burbuja() {
-    let temp, i, j, comparaciones=0, intercambios=0;
+    let temp, i, j;
     for (i = 1; i < this.size; i++) {
-      comparaciones++;
       for (j = 0; j < this.size - i; j++) {
         if (this.array[j] > this.array[j + 1]) {
           temp = this.array[j];
           this.array[j] = this.array[j + 1];
           this.array[j + 1] = temp;
-          intercambios++;
         }
       }
     }
-    return this.Show()+'\ncomparaciones: '+comparaciones+'\nintercambios: '+intercambios;
   }
   BurbujaMejorado() {
-    let i,flag=1,j,aux,comparaciones=0,intercambios=0;
-    for(i=0;i<this.size-1 && flag==1;i++){
-      flag=0;
-      comparaciones++;
-      for(j=0;j<this.size-i-1;j++){
-        if(this.array[j]<this.array[j+1]){
-          flag=1;
-          aux=this.array[j];
-          this.array[j]=this.array[j+1];
-          this.array[j+1]=aux;
-          intercambios++;
+    let i,
+      flag = 1,
+      j,
+      aux;
+    for (i = 0; i < this.size - 1 && flag == 1; i++) {
+      flag = 0;
+      for (j = 0; j < this.size - i - 1; j++) {
+        if (this.array[j] < this.array[j + 1]) {
+          flag = 1;
+          aux = this.array[j];
+          this.array[j] = this.array[j + 1];
+          this.array[j + 1] = aux;
         }
       }
     }
-    return this.Show()+'\ncomparaciones: '+comparaciones+'\nintercambios: '+intercambios;
   }
-  Monticulo() {
-    return this.Show();
+  Heapsort() {
+    let n = this.array.length;
+    for (let i = Math.floor(n / 2 - 1); i >= 0; i--) this.Heapify(n, i);
+    for (let i = n - 1; i >= 0; i--) {
+      this.Swap(0, i);
+      this.Heapify(i, 0);
+    }
+  }
+  Heapify(length, i) {
+    // i=length/2-1
+    let largest = i,
+      left = 2 * i + 1,
+      right = 2 * i + 2;
+    if (left < length && this.array[left] > this.array[largest]) largest = left;
+    if (right < length && this.array[right] > this.array[largest])
+      largest = right;
+    if (largest !== i) {
+      this.Swap(i, largest);
+      this.Heapify(length, largest);
+    }
+  }
+  Swap(pos1, pos2) {
+    let aux = this.array[pos1];
+    this.array[pos1] = this.array[pos2];
+    this.array[pos2] = aux;
   }
   BusquedaSecuencial(number) {
-    let pos=null,i;
-    for(i=0;i<this.size;i++){
-      if(this.array[i]==number)
-        pos=i;
+    let pos = -1,
+      i;
+    for (i = 0; i < this.size; i++) {
+      if (this.array[i] == number) pos = i;
     }
     return pos;
   }
   BusquedaBinaria(number) {
-    let min=0,med=this.size/2,max=this.size,i,pos=null;
-    for(i=0;i<this.size;i++){
-      if(this.array[med]===number)
-        pos=med;
-      else if(this.array[med]>number){
-          med=(min+max)/2;
-          min=med+1;
-        }
-      else{
-        max=med-1;
-        med=(min+max)/2;
+    let min = 0,
+      med = this.size / 2,
+      max = this.size,
+      i,
+      pos = -1;
+    while (max >= min) {
+      if (this.array[med] === number) pos = med;
+      else if (this.array[med] > number) {
+        med = (min + max) / 2;
+        min = med + 1;
+      } else {
+        max = med - 1;
+        med = (min + max) / 2;
       }
     }
     return pos;
@@ -84,15 +101,18 @@ class Info {
 let arreglo = new Info(20);
 arreglo.RandomFill();
 console.log(arreglo.Show());
-console.log(arreglo.Burbuja());
+arreglo.Burbuja();
+console.log("Ordenado por Burbuja " + arreglo.Show());
 
 arreglo.RandomFill();
 console.log(arreglo.Show());
-console.log(arreglo.BurbujaMejorado());
+arreglo.BurbujaMejorado();
+console.log("Ordenado por Burbuja Mejorado " + arreglo.Show());
 
-/*arreglo.RandomFill();
+arreglo.RandomFill();
 console.log(arreglo.Show());
-console.log(arreglo.Monticulo());*/
+arreglo.Heapsort();
+console.log("Ordenado por heapsort " + arreglo.Show());
 
 arreglo.RandomFill();
 console.log(arreglo.Show());
